c33ada17fc820e283965b13c87b8fa532ec5c00a
David Kahler <phizatmonster@yahoo.com>
2023-02-24 18:26:38 -0500

    More logic

--- lib/init.rb
+++ lib/init.rb
@@ -2,7 +2,7 @@ require "csv"
 require "watir"
 
 # Only scrape values for Steamer, ATC, THE BAT, THE BAT X
-PROJECTION_ROWS = ["3","5","6","7"]
+PROJECTION_ROWS = ["3","5","6","7","8"]
 
 # Set columns to scripr for Hitters and Pitchers
 HITTER_COLUMNS = ["4", "5", "6", "7", "8", "9", "15"]
@@ -43,14 +43,28 @@ def aggregate_custom_player_metrics(player_id="", player_name="", player_type="P
     player_type == "P" ? grid_columns = PITCHER_COLUMNS : grid_columns = HITTER_COLUMNS
     
     player_output = [player_id, player_name]
+    negative_check = nil
     grid_columns.each_with_index do |column, i|
         metric_values = []
         PROJECTION_ROWS.each do |row|
-            grid_cell = @browser.element(:xpath => "//*[@id=\"MainContent_grid#{projection_type}Projections\"]/tbody/tr[#{row}]/td[#{column}]")
-            # Bump $ value for pitchers by 115%
+            grid_cell = @browser.element(:xpath => "//*[@id=\"MainContent_grid#{projection_type}Projections\"]/tbody/tr[#{row}]/td[#{column}]")   
+            grid_cell.text.include?("(") ? negative_check = true : negative_check = false 
+
+            # Adjust pitcher value up 5% and hitter value down 5%
             if player_type == "P" && i == 6
-                cell_value = grid_cell.text.gsub(/[^0-9\.]/, "").to_f
-                cell_value += (cell_value.abs * 0.15)
+                if negative_check
+                    cell_value = -grid_cell.text.gsub(/[^0-9\.]/, "").to_f
+                else
+                    cell_value = grid_cell.text.gsub(/[^0-9\.]/, "").to_f
+                end
+                cell_value = cell_value + (cell_value.abs * 0.05)
+            elsif player_type == "B" && i == 6
+                if negative_check
+                    cell_value = -grid_cell.text.gsub(/[^0-9\.]/, "").to_f
+                else
+                    cell_value = grid_cell.text.gsub(/[^0-9\.]/, "").to_f
+                end
+                cell_value = cell_value - (cell_value.abs * 0.05)
             else
                 cell_value = grid_cell.text.gsub(/[^0-9\.]/, "").to_f
             end
@@ -76,27 +90,26 @@ all_player_names =  all_player_names.gsub("},", "|")
 all_player_names =  all_player_names.gsub("}", "")
 all_player_names =  all_player_names.gsub("{", "")
 all_player_names =  all_player_names.gsub(":", ",")
-
+all_player_names =  all_player_names.gsub("\"", "")
 
 all_player_array = all_player_names.split("|")
-# puts all_player_array
-puts aggregate_custom_player_metrics("592450", "Aaron Judge", "B")
-#puts aggregate_custom_player_metrics("669203", "Corbin Burnes", "P")
 
-player_values_array = [
-    ["669203","Corbin Burnes","188.5","13.0","3.1275","1.0725","227.75","0.0","28.114625"],
-    ["592450","Aaron Judge","549.0","106.5","45.0","111.0","9.25","0.28025","48.5875"]
-]
-
-# Write custom player projections and values to a CSV file
-output_path = File.join(File.dirname(__FILE__), "../data/player_values.csv")
-CSV.open(output_path, "w") do |csv|
-    player_values_array.each { |ar| csv << ar }
+# Write all players to a CSV file
+all_players_path = File.join(File.dirname(__FILE__), "../data/all_players.csv")
+File.open(all_players_path, "w") do |f|
+    all_player_array.each { |element| f.puts(element) }
 end
 
+# Read player_subset.csv into a two-dimensional array
+players_to_project = CSV.read(File.join(File.dirname(__FILE__), "../data/player_subset.csv"))
 
-breakpoint = gets
+# Write custom player projections and values to a CSV file
+player_values_path = File.join(File.dirname(__FILE__), "../data/player_values.csv")
+CSV.open(player_values_path, "ab") do |csv|
+    players_to_project.each do |player|
+        csv << aggregate_custom_player_metrics(player[0].to_s, player[1].to_s, player[2].to_s)
+    end
+end
+
 @browser.close
-
-# {"value":"687659","label":"Kaden Polcovich","PlayerType":"B","Position":"SS","TeamID":"SEA"}
 
